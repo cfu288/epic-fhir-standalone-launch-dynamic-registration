d2e416a65e7c0b573d159242cd8013d90b53f65a
Christopher Fu <invent288cfu@gmail.com>
2022-12-22 12:13:53 -0500

    feat: add video of bug to demo page

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -10,6 +10,13 @@ export default function App() {
         <a href={getLoginUrl()}>
           <button type="button">Login to MyChart</button>
         </a>
+        <p>
+          This demo is an example of a bug with Epic's dynamic registration.
+          Below is a video of the bug in action:
+        </p>
+        <video controls>
+          <source src="./bug.mp4" type="video/mp4" />
+        </video>
       </main>
     </div>
   );
