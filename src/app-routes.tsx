708540ea8c5081c0b7cefbb2ba3be58cdd2e6977
Christopher Fu <invent288cfu@gmail.com>
2022-12-22 12:22:51 -0500

    build: update vite base url

--- src/app-routes.tsx
+++ src/app-routes.tsx
@@ -1,4 +1,4 @@
-export enum AppRoutes {
-  Home = "/",
-  EpicCallback = "/epic/callback",
-}
+export const AppRoutes = {
+  Home: `${import.meta.env.BASE_URL}/`,
+  EpicCallback: `${import.meta.env.BASE_URL}/epic/callback`,
+};
