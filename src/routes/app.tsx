523ba36c3e0dab2a992a71a14baeae3bf5b592b8
Christopher Fu <invent288cfu@gmail.com>
2022-12-22 22:28:20 -0500

    feat: remove video from demo page

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -10,16 +10,6 @@ export default function App() {
         <a href={getLoginUrl()}>
           <button type="button">Login to MyChart</button>
         </a>
-        <p>
-          This demo is an example of a bug with Epic's dynamic registration.
-          Below is a video of the bug in action:
-        </p>
-        <video controls>
-          <source
-            src="https://user-images.githubusercontent.com/2985976/209190730-d38774e3-2f56-4756-9c75-2f044654bd40.mp4"
-            type="video/mp4"
-          />
-        </video>
       </main>
     </div>
   );
